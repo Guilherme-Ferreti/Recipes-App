7fb918ab820c57921e7ac5559bc21e8f4a02460e
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-29 17:25:23 -0300

    Added deploy to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), eslintPlugin()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/scss/globals.scss";',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [vue(), vueDevTools(), eslintPlugin()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/globals.scss";',
+        },
+      },
+    },
+    base: env.VITE_BASE_URL,
+  });
 });
